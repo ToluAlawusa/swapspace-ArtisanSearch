02ca63fbf6eafb9b4342b78ed484daf4cd7d5b78
Tolu Alawusa <tolulopealawusa@gmail.com>
2017-02-17 16:42:01 +0100

    logout controller and route done, header view page adjusted to accommodate the navigation routes

--- www/laravel/resources/views/includes/adminheader.blade.php
+++ www/laravel/resources/views/includes/adminheader.blade.php
@@ -3,7 +3,10 @@
         <h1>Artisan<span>Hub</span></h1>
         <nav>
             <ul class="clearfix">
-                <li><a href="">
+                <li><a href="{{ route('addcategory') }}" class="selected">ADD CATEGORY</a></li>
+                <li><a href="{{ route('viewcategory') }}" class="selected">VIEW CATEGORY</a></li>
+                <li><a href="{{ route('admindashboard') }}" class="selected">DASHBOARD</a></li>
+                <li><a href="{{ route('adminlogout') }}" class="selected">LOGOUT</a></li>
                
             </ul>
         </nav>
